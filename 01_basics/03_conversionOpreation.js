0a210bf1f39dd28478b1214335ff597f5b369a9f
Akshaykashyap <Akshay95kashyap@gmail.com>
2023-11-29 07:16:53 +0000

    Conversion Operations

--- 01_basics/03_conversionOpreation.js
+++ 01_basics/03_conversionOpreation.js
@@ -1,11 +1,11 @@
 // string to Number
 
 let score = "56";
-console.log(typeof(score));
+// console.log(typeof(score));
 
 var valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "56" => 56
 // "56abc" => NaN
@@ -18,8 +18,8 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // " " => false
@@ -31,5 +31,26 @@ let someNumber = 54;
 
 let stringNumber = String(someNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// *********************** Operations ***************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Akshay";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2) // 122
+console.log(1+ 2 + 4 + "5"); // 75
+
+
+let gamecounter = 100;
+++gamecounter;
+console.log(gamecounter);
