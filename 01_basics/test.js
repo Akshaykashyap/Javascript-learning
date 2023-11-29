5ccdeb079d8da9251f56776caa5d873a417ecc9a
Akshaykashyap <Akshay95kashyap@gmail.com>
2023-11-29 05:38:40 +0000

    variable declaration

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+const accountId =  2345643
+let accountEmail = "Ak@google.com"
+var accountPassword = "123456"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2  // not allowed
+
+console.log(accountId,accountEmail,accountPassword,accountCity);
+
+accountEmail = "Ak@ak.com"
+accountPassword = "31313131"
+accountCity = "Dharamshala"
+
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
