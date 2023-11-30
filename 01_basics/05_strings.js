b948fc4cc914e12746be42025eccebb3116c7748
Akshaykashyap <Akshay95kashyap@gmail.com>
2023-11-30 11:37:37 +0000

    String functions

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -28,3 +28,50 @@ console.log(url.includes('kashyap'));
 
 const gameNameOne = new String('Akshay-kashyap-Javascript');
 console.log(gameNameOne.split('-'));
+
+const sentence = 'Akshaykashyap.';
+
+console.log(`Character code ${sentence.charCodeAt(2)} `);
+
+const array1 = ['Akshay', 'Kashyap'];
+const array2 = ['28', 'kagnra', 'Himachal pradesh'];
+const array3 =`Hello Good Morning My Name is ${array1.concat(array2)}`;
+
+// document.body.innerHTML = array3;
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
+
+console.log(found);
+
+
+const str1 = 'Breaded Mushrooms';
+
+console.log(str1.padEnd(20, '.'));
+
+const str21 = '5';
+
+console.log(str21.padStart(2, '0'));
+// Expected output: "05"
+
+const fullNumber = '2034399002125581';
+const last4Digits = fullNumber.slice(-4);
+const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+console.log(maskedNumber);
+
+const mood = "Happy! ";
+console.log(`Today I am ${mood.repeat(2)}`);
+
+const para = "I think Ruth's dog is cuter than your dog!";
+
+console.log(para.replace("Ruth's", "My"));
+
+const reg = /Dog/i;
+console.log(para.replace(reg, 'ferret'));
+
+const re = /apples/gi;
+const str = "Apples are round, and apples are juicy.";
+const newstr = str.replace(re, "oranges");
+// document.body.innerHTML = newstr;
