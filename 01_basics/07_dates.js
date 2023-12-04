917b334ae80e35be442d3d2af53436b54f43c6c1
Akshaykashyap <Akshay95kashyap@gmail.com>
2023-12-04 10:23:13 +0000

    Dates and Time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,25 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+//let myCreateDate = new Date(2023,0,23);
+// let myCreateDate = new Date(2023,0,23,5,3);
+let myCreateDate = new Date("01-14-2023");
+console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+console.log(myCreateDate.getDay());
+
+let newValu = myDate.toLocaleTimeString('default', {
+    weekday: "long",
+    day: "2-digit",
+})
+console.log(newValu);
